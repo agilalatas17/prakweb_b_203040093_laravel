ceff302accd742a1d669dfa8d8684fafda8c7007
Agil_Alatas <agilalatas17@gmail.com>
2022-10-30 13:31:29 +0700

    Post Category dan Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 
 @section('container')  
     <h1>{{ $post->title }}</h1>
-    
+    <p>By Agil Alatas in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
     {!! $post->body !!}
 
     <a href="/posts">Back to Posts</a>
