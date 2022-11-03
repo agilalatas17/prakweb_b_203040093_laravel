8703e6ff43ab7e9e7b76fd017d0311710750ce80
Agil_Alatas <agilalatas17@gmail.com>
2022-11-03 19:02:52 +0700

    Searching dan Pagination

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,7 +9,7 @@
 
                 <img src="https://source.unsplash.com/1200x400?{{$post->category->name}}" alt="{{$post->category->name}}" class="img-fluid">
 
-                <p>By <a href="/authors/{{$post->author->username}}" class="text-decoration-none">{{$post->author->name}}</a> in <a class="text-decoration-none" href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
+                <p>By <a href="/posts?author={{$post->author->username}}" class="text-decoration-none">{{$post->author->name}}</a> in <a class="text-decoration-none" href="/post?category={{$post->category->slug}}">{{$post->category->name}}</a></p>
                   
                 <article class="my-3 fs-5">
                     {!! $post->body !!}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,24 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">{{$title}}</h1>
+    <h1 class="mb-3 text-center">{{$title}}</h1>
+
+    <div class="row justify-content-center mb-3">
+        <div class="col-md-6">
+            <form action="/posts">
+                @if(request('category'))
+                    <input type="hidden" name="category" value="{{request('category')}}">
+                @endif
+                @if(request('author'))
+                    <input type="hidden" name="author" value="{{request('author')}}">
+                @endif
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search..." name="search" value="{{request('search')}}">
+                    <button class="btn btn-primary" type="submit">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
     @if ($posts->count())
         {{-- Card --}}
@@ -12,36 +29,43 @@
             <div class="card-body">
                 <h5 class="card-title"><a href="/posts/{{$posts[0]->slug}}" class="text-decoration-none text-dark">{{$posts[0]->title}}</a></h5>
                 <p>
-                    By <a href="/authors/{{$posts[0]->author->username}}" class="text-decoration-none">{{$posts[0]->author->name}}</a> in <a href="/categories/{{$posts[0]->category->slug}}" class="text-decoration-none">{{$posts[0]->category->name}}</a>
+                    By <a href="/posts?author={{$posts[0]->author->username}}" class="text-decoration-none">{{$posts[0]->author->name}}</a> in <a href="/posts?category={{$posts[0]->category->slug}}" class="text-decoration-none">{{$posts[0]->category->name}}</a>
                     <small class="text-muted">{{$posts[0]->created_at->diffForHumans()}}</small>
                 </p>
                 <p class="card-text">{{$posts[0]->excerpt}}</p>
                 <a href="/posts/{{$posts[0]->slug}}" class="text-decoration-none btn btn-primary">Read More</a>
             </div>
         </div>
+    
+
+        <div class="container">
+            <div class="row">
+                @foreach($posts->skip(1) as $post)
+                <div class="col-md-4 mb-3" >
+                    <div class="card">
+                        <div class="position-absolute m-2"><a href="/posts?category={{$post->slug}}" class="badge rounded-pill bg-warning text-dark text-decoration-none">{{$post->category->name}}</a></div>
+                        <img src="https://source.unsplash.com/500x400?{{$post->category->name}}" class="card-img-top" alt="{{$post->category->name}}">
+                        <div class="card-body overflow-hidden">
+                            <h5 class="card-title text-truncate">{{ $post->title }}</h5>
+                            <p>
+                                By <a href="/posts?author={{$post->author->username}}" class="text-decoration-none">{{$post->author->name}}</a>
+                                <small class="text-muted">{{$post->created_at->diffForHumans()}}</small>
+                            </p>
+                            <p class="card-text ">{{ $post->excerpt }}</p>
+                            <a href="/posts/{{$post->slug}}" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+
     @else 
         <p class="text-center fs-4">No Post Found.</p>
     @endif
 
-    <div class="container">
-        <div class="row">
-            @foreach($posts->skip(1) as $post)
-            <div class="col-md-4 mb-3" >
-                <div class="card">
-                    <div class="position-absolute m-2"><a href="/posts/{{$post->slug}}" class="badge rounded-pill bg-warning text-dark text-decoration-none">{{$post->category->name}}</a></div>
-                    <img src="https://source.unsplash.com/500x400?{{$post->category->name}}" class="card-img-top" alt="{{$post->category->name}}">
-                    <div class="card-body overflow-hidden">
-                        <h5 class="card-title text-truncate">{{ $post->title }}</h5>
-                        <p>
-                            By <a href="/authors/{{$post->author->username}}" class="text-decoration-none">{{$post->author->name}}</a>
-                            <small class="text-muted">{{$post->created_at->diffForHumans()}}</small>
-                        </p>
-                        <p class="card-text ">{{ $post->excerpt }}</p>
-                        <a href="/posts/{{$post->slug}}" class="btn btn-primary">Read More</a>
-                    </div>
-                </div>
-            </div>
-            @endforeach
-        </div>
+    {{-- Pagination --}}
+    <div class="d-flex justify-content-center">
+        {{$posts->links()}}
     </div>
 @endsection
