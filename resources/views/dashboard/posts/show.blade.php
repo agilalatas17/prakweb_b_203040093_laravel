96cde1f4bd6082df2744e5054a214efeac1ffde0
Agil_Alatas <agilalatas17@gmail.com>
2022-11-30 21:04:52 +0700

    Update & Delete Post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,8 +7,12 @@
       <h1 class="mb-3">{{ $posts->title }}</h1>
 
       <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my posts</a>
-      <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
-      <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete </a>
+      <a href="/dashboard/posts/{{ $posts->slug}}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+      <form action="/dashboard/posts/{{ $posts->slug }}" method="post" class="d-inline">
+        @method('delete')
+        @csrf
+        <button class="btn btn-danger border-0" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span>Delete</button>
+      </form>
 
       <img src="https://source.unsplash.com/1200x400?{{ $posts->category->name }}" alt="{{ $posts->category->name }}" class="img-fluid mt-3">
 
